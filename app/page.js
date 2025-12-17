e510998c2b05ad326785816a7e1ef3f712828190
Ujer123 <ujermohd0@gmail.com>
2025-12-17 09:56:11 +0530

    seo

--- app/page.js
+++ app/page.js
@@ -1,9 +1,33 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchSEO } from '@/lib/slices/seoSlice';
 import HomeCard from "@/components/HomeCard";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const { seoData, loading, error } = useSelector((state) => state.seo);
+
+  useEffect(() => {
+    dispatch(fetchSEO('homepage'));
+  }, [dispatch]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
-<>
-<HomeCard />
-</>
+    <>
+      {seoData && (
+        <div className="p-3">
+          <h1>{seoData.h1}</h1>
+          <p>{seoData.description}</p>
+        </div>
+      )}
+      <HomeCard />
+      {seoData && (
+        <div className='p-3' dangerouslySetInnerHTML={{ __html: seoData.content }} />
+      )}
+    </>
   );
 }
